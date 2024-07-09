a69611e11eabd0903b964ada6093414214e8d426
ras0q <ras0q@users.noreply.github.com>
2024-07-10 08:05:39 +0900

    :sparkles: add feed title

--- _config.ts
+++ _config.ts
@@ -4,7 +4,14 @@ import wiki from "wiki/mod.ts";
 
 const site = lume();
 
-site.use(feed());
+site.use(
+  feed({
+    info: {
+      title: "Ras's diary",
+      lang: "ja",
+    },
+  })
+);
 site.use(wiki());
 
 export default site;
