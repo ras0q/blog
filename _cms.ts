b0bbf5ffed851cb3c8ae35ea871d72f845a1ecc8
ras0q <ras0q@users.noreply.github.com>
2024-09-25 03:02:27 +0900

    :sparkles: add basic auth

--- _cms.ts
+++ _cms.ts
@@ -2,16 +2,31 @@ import lumeCMS from "lume/cms/mod.ts";
 import GitHub from "lume/cms/storage/github.ts";
 import { Octokit } from "octokit";
 
-const cms = lumeCMS();
+const username = Deno.env.get("CMS_USERNAME");
+const password = Deno.env.get("CMS_PASSWORD");
+const isRemote = username && password;
 
-cms.storage(
-  "src",
-  new GitHub({
-    client: new Octokit({ auth: Deno.env.get("GITHUB_TOKEN") }),
-    owner: "ras0q",
-    repo: "diary",
-  })
-);
+const cms = lumeCMS({
+  auth: isRemote
+    ? {
+        method: "basic",
+        users: {
+          [username]: password,
+        },
+      }
+    : undefined,
+});
+
+if (isRemote) {
+  cms.storage(
+    "src",
+    new GitHub({
+      client: new Octokit({ auth: Deno.env.get("GITHUB_TOKEN") }),
+      owner: "ras0q",
+      repo: "diary",
+    })
+  );
+}
 
 cms.collection("pages: All pages of the site", "src:**/*.md", [
   "date: datetime",
