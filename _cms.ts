cfa57beb666cf96737f45e0c6e5c6e6e55d746c9
ras0q <ras0q@users.noreply.github.com>
2024-09-25 02:41:11 +0900

    :sparkles: add cms

--- _cms.ts
+++ _cms.ts
@@ -1,3 +1,23 @@
-import cms from "wiki/_cms.ts";
+import lumeCMS from "lume/cms/mod.ts";
+import GitHub from "lume/cms/storage/github.ts";
+import { Octokit } from "octokit";
+
+const cms = lumeCMS();
+
+cms.storage(
+  "src",
+  new GitHub({
+    client: new Octokit({ auth: Deno.env.get("GITHUB_TOKEN") }),
+    owner: "ras0q",
+    repo: "diary",
+  })
+);
+
+cms.collection("pages: All pages of the site", "src:**/*.md", [
+  "date: datetime",
+  "content: markdown",
+]);
+
+cms.upload("uploads: Uploaded files", "src:assets");
 
 export default cms;
