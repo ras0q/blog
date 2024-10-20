cff9d456b27cd5b4c2320abe9f4050dabdc8ce93
ras0q <ras0q@users.noreply.github.com>
2024-10-20 12:02:08 +0900

    :lipstick: use absolute date

--- _includes/libs/PageData.tsx
+++ _includes/libs/PageData.tsx
@@ -1,10 +1,9 @@
-import { calcDuration } from "./duration.ts";
 import Tag from "./Tag.tsx";
 
 export default ({ tags, date }: { tags: string[]; date: Date }) => (
   <div class="py-xs space-y-lg">
     <div class="flex gap-xs text-sm">
-      <p class="opacity-60">{`${calcDuration(date)}日前`}</p>
+      <p class="opacity-60">{date.toLocaleDateString("sv-SE")}</p>
       {tags.map((tag) => <Tag tag={tag} />)}
     </div>
   </div>
--- _includes/libs/duration.ts
+++
@@ -1,2 +0,0 @@
-export const calcDuration = (from: Date, to: Date = new Date()): number =>
-  Math.floor((to.valueOf() - from.valueOf()) / (24 * 60 * 60 * 1000));
