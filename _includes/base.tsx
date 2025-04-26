82a1c2b300207e2e179557a8e2656e5267dd1521
ras0q <ras0q@users.noreply.github.com>
2025-04-26 19:06:04 +0900

    set target to external links

--- _includes/base.tsx
+++ _includes/base.tsx
@@ -14,8 +14,14 @@ export default (data: Lume.Data) => (
       <nav class="p-4xl flex flex-wrap justify-between">
         <a href="/" class="text-xl font-bold decoration-none">{siteTitle}</a>
         <div class="space-x-xl">
-          <a href="https://github.com/ras0q/blog">GitHub</a>
-          <a href="/feed.rss">RSS</a>
+          <a
+            href="https://github.com/ras0q/blog"
+            target="_blank"
+            rel="noopener"
+          >
+            GitHub
+          </a>
+          <a href="/feed.rss" target="_blank" rel="noopener">RSS</a>
         </div>
       </nav>
       <main
