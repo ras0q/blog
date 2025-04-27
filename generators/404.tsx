58e4464e301dfadafd260b99eb0e7a756e85722b
ras0q <ras0q@users.noreply.github.com>
2025-04-27 13:37:04 +0900

    rewrite with tsx

---
+++ generators/404.tsx
@@ -0,0 +1,10 @@
+export const layout = "base.tsx"
+export const url = "/404.html"
+export const title = "404"
+
+export default () => (
+  <div>
+    <h1>Content not found!</h1>
+    <p>Go <a href="/">home</a>.</p>
+  </div>
+);
